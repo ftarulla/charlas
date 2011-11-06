a079309db01daeb50be5b0fbd355537c64f3cc75
ftarulla <ftarulla@gmail.com>
2011-11-06 15:55:20 -0300

    schema oradores

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111106172834) do
+ActiveRecord::Schema.define(:version => 20111106184428) do
 
   create_table "charlas", :force => true do |t|
     t.string   "titulo"
@@ -19,6 +19,22 @@ ActiveRecord::Schema.define(:version => 20111106172834) do
     t.datetime "hora"
     t.string   "sala"
     t.integer  "orador_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "oradores", :force => true do |t|
+    t.string   "nombre"
+    t.string   "twitter"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "oradors", :force => true do |t|
+    t.string   "nombre"
+    t.string   "twitter"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
